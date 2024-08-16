917e23ffa2c0d14c8d3f239b4a2d3919df93af3f
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-08-16 21:02:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const divInfo = document.createElement('div');
+divInfo.setAttribute("title","titleinfo");
+divInfo.className="div-info";
+divInfo.id="dev-info-id"
+divInfo.style.textAlign="center"
+divInfo.innerHTML="Rakesh Website"
+document.body.append(divInfo)
+console.log(divInfo);
+console.log(document.body);
+
+
