1b801af08ddcea0ee99f9a2cc17e95eb5f20000a
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-08-18 21:53:36 +0530

    practical_learrning

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+
+
 const divInfo = document.createElement('div');
 divInfo.setAttribute("title","titleinfo");
 divInfo.className="div-info";
 divInfo.id="dev-info-id"
 divInfo.style.textAlign="center"
 divInfo.innerHTML="Rakesh Website"
-document.body.append(divInfo)
-console.log(divInfo);
-console.log(document.body);
+body.appendChild(divInfo)
+// console.log(divInfo);
+// console.log(document.body);
 
+
+//++++++++++++++++++++++++++++++ input field +++++++++++++++++++++++++++++++++++++++++
+
+
+let inputfieldEntry = document.getElementById("center-input")
+console.log(inputfieldEntry);
 
