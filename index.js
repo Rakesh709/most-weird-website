8d94d7469074426f21af7755cceca485f6ac25da
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-08-23 01:00:32 +0530

    addeed color picker

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-
-
-const divInfo = document.createElement('div');
-divInfo.setAttribute("title","titleinfo");
-divInfo.className="div-info";
-divInfo.id="dev-info-id"
-divInfo.style.color="white"
-divInfo.style.textAlign="center"
-divInfo.innerHTML="Rakesh Website"
-body.appendChild(divInfo)
+const divInfo = document.createElement("div");
+divInfo.setAttribute("title", "titleinfo");
+divInfo.className = "div-info";
+divInfo.id = "dev-info-id";
+divInfo.style.color = "white";
+divInfo.style.textAlign = "center";
+divInfo.innerHTML = "Rakesh Website";
+body.appendChild(divInfo);
 // console.log(divInfo);
 // console.log(document.body);
 
-
 //++++++++++++++++++++++++++++++ input field +++++++++++++++++++++++++++++++++++++++++
-
 
 // let inputfieldEntry = document.getElementById("center-input").addEventListener('click',function(e){
 //     console.log("something entered")
@@ -22,36 +18,64 @@ body.appendChild(divInfo)
 
 //+++++++++++++++++ CLOCK ++++++++++++++++++++++++++++++++
 
-const istClock = document.getElementById("ist-clock")
+const istClock = document.getElementById("ist-clock");
 
-setInterval(function(){
-    const date = new Date();
-    istClock.innerHTML = date.toLocaleTimeString()
-},1000)
+setInterval(function () {
+  const date = new Date();
+  istClock.innerHTML = date.toLocaleTimeString();
+}, 1000);
 
+const ustClock = document.getElementById("UST-clock");
 
-const ustClock = document.getElementById("UST-clock")
+setInterval(function () {
+  const date = new Date();
+  ustClock.innerHTML = date.toLocaleTimeString("en-US", {
+    timeZone: "America/New_York",
+  });
+}, 1000);
 
-setInterval(function(){
-    const date = new Date();
-    ustClock.innerHTML= date.toLocaleTimeString("en-US", {timeZone: "America/New_York"})
-},1000)
+const bstClock = document.getElementById("BST-clock");
 
-const bstClock = document.getElementById("BST-clock")
+setInterval(function () {
+  const date = new Date();
+  bstClock.innerHTML = date.toLocaleTimeString("en-US", {
+    timeZone: "Europe/London",
+  });
+}, 1000);
 
-setInterval(function(){
-    const date = new Date();
-    bstClock.innerHTML= date.toLocaleTimeString("en-US", {timeZone: "Europe/London"})
-},1000)
-
-
-
-
-function checkInput(){
-    let input = document.getElementById("center-input").value;
-    if(!NaN  && input.trim()!== ''){
-        alert(`input is ${input} is the number`)
-    }else{
-        alert("this is string")
-    }
+function checkInput() {
+  let input = document.getElementById("center-input").value;
+  if (!NaN && input.trim() !== "") {
+    alert(`input is ${input} is the number`);
+  } else {
+    alert("this is string");
+  }
 }
+
+// colour picker
+
+const button = document.querySelectorAll(".button");
+//console.log(button);
+
+const colourBody = document.querySelector(".canvas");
+
+console.log(colourBody);
+
+button.forEach(function (button) {
+  //console.log(button)
+  button.addEventListener("click", function (e) {
+    //console.log(e.target);
+    if (e.target.id === "grey") {
+      colourBody.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      colourBody.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      colourBody.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      colourBody.style.backgroundColor = e.target.id;
+    }
+  });
+});
